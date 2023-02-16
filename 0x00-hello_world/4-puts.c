963d8622be63ea22afe0df533905c1b1e28f0001
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-16 14:20:41 -0800

    New script added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
-int main()
+/**
+ *This is a code that outputs a line
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
