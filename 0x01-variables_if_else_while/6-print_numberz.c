6b355d1bd47ff889e8c517941688e28a8bb19d1f
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-17 10:00:17 -0800

    New script added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - In this program, we use a for loop to iterate over number 1 to 9
+ *
+ *Return: Always 0 (Succss)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
+	return (0);
+}
