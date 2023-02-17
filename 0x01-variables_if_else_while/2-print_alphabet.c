b89013c3483289323b317e6cb97f1242bdb8c8fd
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-17 09:12:00 -0800

    New script added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - In this program, we use a for loop to iterate over the ASCII codes
+ *for the lowercase letters from 'a' to 'z'.
+ *Return : Always 0 (Succss)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
