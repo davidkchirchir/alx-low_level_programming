4cd9a1d5e376b2baa1af2d053ab08ff2a98963b5
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-16 16:03:05 -0800

    New script added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(STDERR_FILENO, message, sizeof(message) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
