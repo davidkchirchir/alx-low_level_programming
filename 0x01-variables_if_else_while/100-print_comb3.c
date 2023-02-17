7eb350ac634177a76cfacd9d0bffe6c5c0523712
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-17 15:39:24 -0800

    New script added

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,14 +20,10 @@ int main(void)
 			{
 				putchar(i);
 				putchar(j);
+	
+				if (i != '8' || j != '9')
 				putchar(',');
 				putchar(' ');
-
-				if (i == '8' && j == '9')
-				{
-					continue;
-					
-				}
 
 			}
 		}
