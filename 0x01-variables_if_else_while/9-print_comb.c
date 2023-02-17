86f820a637d0b074a97bdb3a648ea3d4d5e08303
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-17 12:20:06 -0800

    New script added

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,18 +9,15 @@
  */
 int main(void)
 {
-	int i;
-	int j;
+	int n;
 
-	for (i = 0; i <= 9; i++)
+	for (n = 0; n <= 9; n++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar('\n');
-		}
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
+
 	return (0);
 }
