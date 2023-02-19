ec53133956a6573b466f05adf7657b3f2edbec6e
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-19 12:06:17 -0800

    New script added

--- 0x01-variables_if_else_while/100-print_comb.c
+++ 0x01-variables_if_else_while/100-print_comb.c
@@ -11,28 +11,22 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{	
-			if (i != j)
-			{
-				for (k=18; k>=17; k--)
-				{
-					if (i+j == k)
-					{
-						putchar(i+'0');
-						putchar(j+'0');
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
+		for (j = i + 1; j < 10; j++)
+		{
+
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 8 && j == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
