a10f7a0b2afd2789a58b5c6f3433fefc70e41535
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-21 14:10:02 -0800

    New script added

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  *@c : is a character and functions argument
  * Return: 1 for uppercase or 0 for lowercase
  */
-int _isalphai(int c)
+int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
 	{
