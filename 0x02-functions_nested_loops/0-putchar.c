4d5d5dc1f020a3e29a51ba23577c76a58cf6ef70
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-21 12:56:28 -0800

    New script added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,11 @@
 int main(void)
 {
 	char str[] = "_putchar";
-	int len = sizeof(str);
 
-	/*for (i = 0; i < len; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(str[i]);
 	}
-	*/
-	_putchar(len);
+	_putchar(8);
 	return (0);
 }
