5c4d708eb65ca69eaab97bcf34439e7baf2f0b11
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-21 15:57:56 -0800

    New script added

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,30 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+*times_table - prints the 9 times table
+*
+*Return: returns nothing
+*/
+void times_table(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	int number, multiplier, result;
+
+	for (number = 0; number <= 9; number++)
+	{
+		_putchar('0');
+		for (multiplier = 1; multiplier <= 9; multiplier++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			result = number * multiplier;
+			if (result <= 9)
+			{
+				_putchar(' ');
+			else
+			{
+				_putchar((result / 10) + '0');
+			}
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
