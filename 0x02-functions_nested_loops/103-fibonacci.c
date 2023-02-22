e714c595299258ae8f63a7d8ba5ab9a4a6c6219b
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-22 14:10:31 -0800

    New script added

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,24 +7,21 @@
  */
 int main(void)
 {
-    int limit = 4000000;
-    int sum = 0;
-    int fib1 = 1, fib2 = 2, fib3;
+	int limit = 4000000;
+	int sum = 0;
+	int fib1 = 1, fib2 = 2, fib3;
 
-    while (fib2 <= limit)
-    {
-        if (fib2 % 2 == 0)
-        {
-            sum += fib2;
-        }
-
-        fib3 = fib1 + fib2;
-        fib1 = fib2;
-        fib2 = fib3;
-    }
-
-    printf("%d\n", sum);
-
-    return (0);
+	while (fib2 <= limit)
+	{
+		if (fib2 % 2 == 0)
+		{
+			sum += fib2;
+		}
+		fib3 = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fib3;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
 
