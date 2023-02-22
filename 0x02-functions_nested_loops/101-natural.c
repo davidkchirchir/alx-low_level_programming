d96bddcb655b8729cf05910527d6e383fb9d1329
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-22 13:29:30 -0800

    New script added

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,7 +4,6 @@
 int main(void)
 {
 	int sum_of_multiples = 0;
-	int counter;
 	int number;
 
 	for (number = 1; number > 10; number++)
@@ -12,11 +11,11 @@ int main(void)
 		if(number % 3 == 0 || number % 5 == 0)
 		{
 			sum_of_multiples += number;
-			counter++;
+			number++;
 		}
 		else
 		{
-			counter++;
+			number++;
 		}
 	}
 	printf("%d\n",sum_of_multiples);
