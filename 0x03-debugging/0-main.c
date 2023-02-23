5f0c23c9e88e7f46a9dc4e33f8adbb8e8f104fb9
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-23 13:34:33 -0800

    New script added

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*main - This is the main function. Tests if
+*number is positive or negative
+*Return : return 0
+*/
+
+int main (void)
+{
+	int number;
+
+	number = 0;
+	positive_or_negative(number);
+
+	return (0);
+}
