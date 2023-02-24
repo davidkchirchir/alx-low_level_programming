30122c74a5aa37deb288c2370f9357ca90ec57d1
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-24 12:06:01 -0800

    New script added

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
