d21f3d64e11a2fe12cdc7859a2b29b34e84c9262
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-27 14:20:36 -0800

    New script added

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _atoi - changes a string to an int
+* @s: the string to be changed
+*
+* Return: the converted int
+*/
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int num = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			sign = -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = num * 10 + (*s - '0');
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+		s++;
+	}
+	return (num * sign);
+}
