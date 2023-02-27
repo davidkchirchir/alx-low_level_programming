9f9310c4136e9119f100d52d750d8fcb1526187b
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-27 11:55:43 -0800

    New script added

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,7 @@
 void print_rev(char *s)
 {
 	int len = 0, index = 0;
-	
+
 	len = _strlen(s);
 
 	for (index = len - 1; index >= 0; index--)
@@ -16,5 +16,23 @@ void print_rev(char *s)
 		_putchar(s[index]);
 	}
 	_putchar('\n');
+}
+
+/**
+*_strlen - returns the length of a string
+*@s: string whose length returned
+*Return: returns string s length ;
+*/
+
+int _strlen(char *s)
+{
+	int count;
+	int s_lenth = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		s_lenth++;
+	}
+	return (s_lenth);
 }
 
