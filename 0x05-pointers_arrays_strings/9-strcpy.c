eb9ca685444c7fd525a3fbd74eda0839211c3073
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-27 13:42:46 -0800

    New script added

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,22 @@
 #include "main.h"
+
 /**
 * _strcpy - Copy paste string
 *@dest: destination
 *@src: source
 *Return: dest
 */
-char *_strcpy(char *dest, char *src)
+
+char *_strcpy(char *dest, const char *src)
 {
-int inc = 0;
-while (*(src + inc) != '\0')
-{
-*(dest + inc) = *(src + inc);
-inc++;
-}
-*(dest + inc) = '\0';
-return (dest);
+	char *dest_start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return dest_start;
 }
