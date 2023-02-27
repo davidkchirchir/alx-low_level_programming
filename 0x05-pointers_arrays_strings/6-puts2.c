d44357e3d1a4ecff8476a9128bf3efa444a238b8
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-27 12:45:30 -0800

    New script added

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,15 @@
 void puts2(char *str)
 {
 	int i = 0;
-	
+
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(str[i]);
-			i++;
+			putchar(str[i]);
 		}
-		_putchar('\n');
-	}
+        }
+        i++;
+    }
+    putchar('\n');
 }
