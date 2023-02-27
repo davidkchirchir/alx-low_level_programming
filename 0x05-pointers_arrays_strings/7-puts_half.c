3c6cd922c611952d63574d2197dbba15809ecf59
David K. Chirchir <david.k.chirchir@gmail.com>
2023-02-27 12:57:27 -0800

    New script added

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 	int length = _strlen(str);
 	int start = (length % 2 == 0) ? (length / 2) : ((length - 1) / 2 + 1);
 
-	for (int i = start; i < length; i++)
+	for(int i = start; i < length; i++)
 	{
 		putchar(str[i]);
 	}
