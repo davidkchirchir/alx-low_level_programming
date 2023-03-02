8cbd762618f1530c0bd495974db82dd5e61b171f
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-02 12:14:54 -0800

    New script added

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+include "main.h"
+
+/**
+ *_strncat - concatenate two strings but add inputted number of bytes
+ *@dest: string to be appended upon
+ *@src: string to be completed at end of dest
+ *@n:integer parameter to compare index to
+ *Return: returns new concatenated string
+ */
+
+char *_strncat(char *dest, const char *src, int n)
+{
+	int i;
+	int j;
+
+	index = 0;
+	
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
