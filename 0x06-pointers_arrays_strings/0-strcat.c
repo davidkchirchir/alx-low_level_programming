2efcf52518625433e93e6ad2a08b84d04034d11a
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-02 12:08:29 -0800

    New script added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,14 +18,14 @@ char *_strcat(char *dest, char *src)
 	{
 		dest_end++;
 	}
-
+	/*Append the source string to the destination string */
 	while (*src != '\0')
 	{
 		*dest_end = *src;
 		dest_end++;
 		src++;
 	}
-
+	/*Add terminating null byte to the end to the concatenated string */
 	*dest_end = '\0';
 	return (dest);
 }
