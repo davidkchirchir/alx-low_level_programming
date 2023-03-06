c1db2a336001fd740590c3475b4833fc4d455973
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-06 13:54:09 -0800

    New script added

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -24,23 +24,3 @@ void print_chessboard(char (*a)[8])
 		putchar('\n');
 	}
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char board[8][8] = {
-        {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-    };
-    print_chessboard(board);
-    return (0);
-}
