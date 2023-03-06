4e606299540420773549207fcd0562c42b692ccc
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-06 14:33:05 -0800

    New script added

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,10 +7,10 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1 = 0, sum2 = 0;
+	int sum1 = 0, sum2 = 0, i;
 
 	/* Iterate over the elements of the matrix and accumulate the sums*/
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum1 += *(a + i * size + i);/* main diagonal*/
 		sum2 += *(a + i * size + (size - i - 1)); /* secondary diagonal*/
