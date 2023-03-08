4f2b6a1e4f838fc8efd6d292ea1a3a3efc47f411
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-07 23:16:05 -0800

    New script added

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,18 +8,19 @@
  */
 int is_prime_number(int n)
 {
+	int i;
+
 	if (n <= 1)
 	{
 		return (0);
 	}
-
-	int i;
 
 	for (i = 2; i <= n / 2; i++)
 	{
 		if (n % i == 0)
 		{
 			return (0);
+		}
 	}
 	return (1);
 }
