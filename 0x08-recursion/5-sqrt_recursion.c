1e6b4e6ff8ec0384ca285ecc784ed964923f62fa
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-07 23:12:52 -0800

    New script added

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,24 +8,24 @@
  */
 int _sqrt_recursion(int n)
 {
-	int i = 1;
-
-	if (n < 0)
-	{
-		return (-1);
-	}
 	if (n == 0 || n == 1)
-	{
 		return (n);
-	}
-	while (i <= n / 2)
-	{
-		if (i * i == n)
-		{
-			return (i);
-		}
-		i++;
-	}
-	return (-1);
+	return (_sqrt(0, n));
 }
 
+/**
+ * _sqrt - returns the square root of a number
+ * @n: test number
+ * @x: squared number
+ *
+ * Return: the square root of n
+ */
+
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
+}
