feaea47bc46ee282833f3c1852f0f21e2d65603e
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-16 14:42:15 -0700

    New script added

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+* _realloc - reallocates a memory block
+* @ptr: pointer to the memory previously allocated with a call to malloc
+* @old_size: size of ptr
+* @new_size: size of the new memory to be allocated
+*
+* Return: pointer to the address of the new memory block
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+	if (new_size >= old_size)
+	{
+		memcpy(new_ptr, ptr, old_size);
+	}
+	else
+	{
+		memcpy(new_ptr, ptr, new_size);
+	}
+	free(ptr);
+	return (new_ptr);
+}
+
