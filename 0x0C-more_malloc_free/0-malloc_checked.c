c5467b013129903f5540ec7bcbeaa6fae32f7ee7
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-16 13:21:17 -0700

    New script added

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,8 +5,6 @@
  * @b: size of memory to allocate.
  *
  * Return: Pointer to the allocated memory.
- *         If malloc fails, the malloc_checked function should cause normal process
- *         termination with a status value of 98.
  */
 void *malloc_checked(unsigned int b)
 {
