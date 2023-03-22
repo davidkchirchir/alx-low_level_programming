0cd0b3b13e30fcc50c4b1bac26baf770869f6718
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-22 13:34:58 -0700

    New script added

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,14 @@
 #include "3-calc.h"
-#include <stdio.h>   /* for printf */
-#include <stdlib.h>  /* for atoi, exit */
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: Always 0.
+*/
 
 int main(int argc, char *argv[])
 {
