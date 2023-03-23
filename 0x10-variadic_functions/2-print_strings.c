9b74739487ff68303b61dffde92fdaded1598841
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-23 07:53:15 -0700

    New script added

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,10 +13,11 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
+	unsigned int i;
 
 	va_start(args, n);
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		char *str = va_arg(args, char *);
 
