9dce83b55f683bc2d1f8839c155efcfe4ddc953f
David K. Chirchir <david.k.chirchir@gmail.com>
2023-03-23 08:25:03 -0700

    New script added

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,13 +16,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (n == 0)
-		return (0);
-	for (i = 0; i < n; i++)
+	if (n != 0)
 	{
-		sum += va_arg(args, int);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(args, int);
+		}
+		va_end(args);
 	}
-	va_end(args);
 	return (sum);
 }
-
