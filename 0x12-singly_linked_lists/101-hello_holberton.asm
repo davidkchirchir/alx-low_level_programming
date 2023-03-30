da08170d64538d32779e287859e201b0fa42bcb0
davidkchirchir <david.k.chirchir@gmail.com>
2023-03-30 15:12:58 -0700

    New script added

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,17 @@
 section .data
-    msg db "Hello, Holberton", 0Ah
-
+    hello: db "Hello, Holberton", 0Ah, 0
+    
 section .text
-    global _start
-
-_start:
-    mov rax, 0                  ; load syscall number for write (0) into rax
-    mov rdi, 1                  ; load file descriptor for stdout (1) into rdi
-    mov rsi, msg                ; load address of message into rsi
-    mov rdx, 16                 ; load length of message into rdx
-    syscall                     ; call write syscall to print message
-
-    mov rax, 60                 ; load syscall number for exit (60) into rax
-    xor rdi, rdi                ; load exit status 0 into rdi
-    syscall                     ; call exit syscall
+    global main
+    
+    extern printf
+    
+main:
+    ; print Hello, Holberton using printf
+    push    qword hello
+    call    printf
+    
+    ; return 0
+    mov     eax, 0
+    ret
 
