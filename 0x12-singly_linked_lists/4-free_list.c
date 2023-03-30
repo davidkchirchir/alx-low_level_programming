fd72c867d613fd4cf3f8b7eea132771085307ae4
davidkchirchir <david.k.chirchir@gmail.com>
2023-03-30 14:31:01 -0700

    New script added

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ *
+ * Return: Always void.
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
